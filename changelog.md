56ae004c15c0721bf894a121f2dcf863c9e3fd1c
Arpit Kumar <triwin2020@gmail.com>
2013-05-29 16:46:18 +0530

    added wordcount and print
    added wordcount and print plugins

--- changelog.md
+++ changelog.md
@@ -1,2 +1,3 @@
 * v0.29 - Added "Post Draft" button.
 * v0.30 - Added option for setting post slug.
+* v0.31 - Added wordcount and print plugin. Optimized Plugin list in the init file.
--- js/init.js
+++ js/init.js
@@ -2,10 +2,6 @@ tinymce.init({
     selector: "textarea.editme",
 	height : "360",
 	browser_spellcheck : true,
-    plugins: [
-        "advlist autolink lists link image charmap preview anchor",
-        "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table paste"
-    ],
+    plugins: "advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen media table paste wordcount print",
     toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code"
 });
